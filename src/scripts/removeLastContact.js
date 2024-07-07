8ed36bcc18afeecf11b3fd3e02c17e61dbb10618
Oleksandr-Zibnytskyi <xander.en.87@gmail.com>
2024-07-07 10:44:20 +0200

    Update removeLastContact.js

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -5,9 +5,13 @@ export const removeLastContact = async () => {
   try {
     const data = await fs.readFile(PATH_DB, 'utf-8');
     const contacts = JSON.parse(data);
+    if (contacts.length > 0) {
     contacts.pop();
     await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
-    console.log('Last contact was removed successfully!');
+    console.log('Last contact was removed successfully!');}
+    else {
+      console.log('No contacts to remove.');
+    }
   } catch (error) {
     console.error('Error reading file!', error);
   }
